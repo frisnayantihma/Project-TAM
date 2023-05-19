01fe92888035bb79970117e445fa457cd305e98b
frisnayantihma <120646460+frisnayantihma@users.noreply.github.com>
2023-05-20 00:35:11 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/project531/Activity/ProfilActivity.java
@@ -0,0 +1,22 @@
+package com.example.project531.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.project531.R;
+
+public class ProfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+
+
+    }
+}
